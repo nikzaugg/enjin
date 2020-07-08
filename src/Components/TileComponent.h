bc8a10d04b1a3a9569875e96720a755fb577c152
Nik Zaugg <zauggnik@gmail.com>
2020-07-08 09:01:10 +0200

    ✨ add TileComponent without Update logic

--- src/Components/TileComponent.h
+++ src/Components/TileComponent.h
@@ -2,6 +2,7 @@
 #define TILECOMPONENT_H
 
 #include <SDL2/SDL.h>
+#include "../AssetManager.h"
 #include "../EntityManager.h"
 #include "../TextureManager.h"
 #include "../../lib/glm/glm.hpp"
@@ -16,9 +17,26 @@ public:
 
     TileComponent(int sourceRectX, int sourceRectY, int x, int y, int tileSize, int tileScale, std::string assetTextureId)
     {
+        texture = Game::assetManager->GetTexture(assetTextureId);
+
+        sourceRectangle.x = sourceRectX;
+        sourceRectangle.y = sourceRectY;
+        sourceRectangle.w = tileSize;
+        sourceRectangle.h = tileSize;
+
+        destinationRectangle.x = x;
+        destinationRectangle.y = y;
+        destinationRectangle.w = tileSize * tileScale;
+        destinationRectangle.h = tileSize * tileScale;
+
+        position.x = x;
+        position.y = y;
     }
 
-    ~TileComponent() {}
+    ~TileComponent()
+    {
+        SDL_DestroyTexture(texture);
+    }
 
     void Initialize() override
     {
@@ -26,6 +44,7 @@ public:
 
     void Update(float deltaTime) override
     {
+        // TODO: here is where we will take care of tile positions based on camera control
     }
 
     void Render() override
