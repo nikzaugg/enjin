ff1ff1886e19f4aafa506577e03523c85ee5594d
Nik Zaugg <zauggnik@gmail.com>
2020-07-08 09:01:38 +0200

    ✨ initialize and load Map from a file

--- src/Game.cpp
+++ src/Game.cpp
@@ -2,6 +2,7 @@
 #include "./Constants.h"
 #include "./Game.h"
 #include "./AssetManager.h"
+#include "./Map.h"
 #include "./Components/TransformComponent.h"
 #include "./Components/SpriteComponent.h"
 #include "./Components/KeyboardControlComponent.h"
@@ -11,6 +12,7 @@ EntityManager manager;
 AssetManager *Game::assetManager = new AssetManager(&manager);
 SDL_Renderer *Game::renderer;
 SDL_Event Game::event;
+Map *map;
 
 Game::Game()
 {
@@ -64,6 +66,10 @@ void Game::LoadLevel(int levelNumber)
     assetManager->AddTexture("tank-image", std::string("./assets/images/tank-big-right.png").c_str());
     assetManager->AddTexture("chopper-image", std::string("./assets/images/chopper-spritesheet.png").c_str());
     assetManager->AddTexture("radar-image", std::string("./assets/images/radar.png").c_str());
+    assetManager->AddTexture("jungle-tiletexture", std::string("./assets/tilemaps/jungle.png").c_str());
+
+    map = new Map("jungle-tiletexture", 1, 32); // call new, as we dynamically create (we need map to survive)
+    map->LoadMap("./assets/tilemaps/jungle.map", 25, 20);
 
     Entity &chopperEntity = manager.AddEntity("chopper");
     chopperEntity.AddComponent<TransformComponent>(140, 106, 0, 0, 32, 32, 1);
--- src/Map.cpp
+++ src/Map.cpp
@@ -1,6 +1,10 @@
+#include <fstream>
 #include "./Game.h"
 #include "Map.h"
 #include "EntityManager.h"
+#include "./Components/TileComponent.h"
+
+extern EntityManager manager;
 
 Map::Map(std::string textureId, int scale, int tileSize)
 {
@@ -11,10 +15,31 @@ Map::Map(std::string textureId, int scale, int tileSize)
 
 void Map::LoadMap(std::string filePath, int mapSizeX, int mapSizeY)
 {
-    // TODO: read the map from the definitions from the .map file
+    std::fstream mapFile;
+    mapFile.open(filePath);
+
+    // loop over columns and rows in the .map file
+    // each row has numbers with comma delimiter
+    // we read the two digits -> 1st: row, 2nd: column
+    for (size_t y = 0; y < mapSizeY; y++)
+    {
+        for (size_t x = 0; x < mapSizeX; x++)
+        {
+            char ch;
+            mapFile.get(ch);
+            int sourceRectY = atoi(&ch) * tileSize;
+            mapFile.get(ch);
+            int sourceRectX = atoi(&ch) * tileSize;
+            AddTile(sourceRectX, sourceRectY, x * (scale * tileSize), y * (scale * tileSize));
+            mapFile.ignore();
+        }
+    }
+
+    mapFile.close();
 }
 
-void Map::AddTile(int sourceX, int sourceY, int x, int y)
+void Map::AddTile(int sourceRectX, int sourceRectY, int x, int y)
 {
-    // TODO: add new tile entity in the game scene
+    Entity &newTile(manager.AddEntity("Tile"));
+    newTile.AddComponent<TileComponent>(sourceRectX, sourceRectY, x, y, tileSize, scale, textureId);
 }
