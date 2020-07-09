57af2a2137f986c4de6456d786566e2d61697900
Nik Zaugg <zauggnik@gmail.com>
2020-07-09 11:27:17 +0200

    ✨ add fonts to AssetManager

--- src/AssetManager.cpp
+++ src/AssetManager.cpp
@@ -7,6 +7,7 @@ AssetManager::AssetManager(EntityManager *manager) : manager(manager)
 void AssetManager::ClearData()
 {
     textures.clear();
+    fonts.clear();
 }
 
 void AssetManager::AddTexture(std::string textureId, const char *fileName)
@@ -14,7 +15,17 @@ void AssetManager::AddTexture(std::string textureId, const char *fileName)
     textures.emplace(textureId, TextureManager::LoadTexture(fileName));
 }
 
+void AssetManager::AddFont(std::string fontId, const char *fileName, int fontSize)
+{
+    fonts.emplace(fontId, FontManager::LoadFont(fileName, fontSize));
+}
+
 SDL_Texture *AssetManager::GetTexture(std::string textureId)
 {
     return textures[textureId];
+}
+
+TTF_Font *AssetManager::GetFont(std::string fontId)
+{
+    return fonts[fontId];
 }
--- src/AssetManager.h
+++ src/AssetManager.h
@@ -3,6 +3,8 @@
 
 #include <map>
 #include <string>
+#include <SDL2/SDL_ttf.h>
+#include "FontManager.h"
 #include "TextureManager.h"
 #include "EntityManager.h"
 
@@ -11,13 +13,18 @@ class AssetManager
 private:
     EntityManager *manager;
     std::map<std::string, SDL_Texture *> textures;
+    std::map<std::string, TTF_Font *> fonts;
 
 public:
     AssetManager(EntityManager *manager);
     ~AssetManager();
     void ClearData();
+
     void AddTexture(std::string textureId, const char *filePath);
+    void AddFont(std::string fontId, const char *filePath, int fontSize);
+
     SDL_Texture *GetTexture(std::string textureId);
+    TTF_Font *GetFont(std::string fontId);
 };
 
 #endif
